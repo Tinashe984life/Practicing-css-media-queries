66463078a061b910b82d5cdb278166763f658848
Tinashe Madanire <madtinashe@gmail.com>
2022-06-06 23:59:10 +0200

    Responsive navbar

---
+++ index.js
@@ -0,0 +1,6 @@
+const toggleb = document.getElementById('toggleb');
+const navb = document.getElementById('nav-links');
+
+toggleb.addEventListener('click', () =>{
+    navb.classList.toggle('active')
+})
